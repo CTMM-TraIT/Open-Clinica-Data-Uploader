ccd6b9b33b03bfb2bc60cb3f5d51e515bcee91dd
Jacob Rousseau <j.rousseau@nki.nl>
2016-09-13 17:51:41 +0200

    Fix for OCDU-154, added the warnings panel to the data-feedback screen and java-script

--- src/main/resources/static/data-feedback.js
+++ src/main/resources/static/data-feedback.js
@@ -11,28 +11,71 @@ var displayMessages = function displayMessages(data) {
 
     }//if
     else {
-        $('#feedback-tables').empty();
-        var error_word = 'errors'; if(data.length == 1) error_word = 'error';
-        var html_title = '<h3><span> <strong>'+data.length +' '+error_word+' found... </strong> </span></h3>';
-        $('#feedback-tables').append(html_title);
-
+        $('#feedback-errors').empty();
+        $('#feedback-warnings').empty();
+        var numberOfErrors = 0;
         for (var i = 0; i < data.length; i++) {
             var fb = data[i];
-            var msg = fb['message'];
-            var vals = fb['offendingValues'];
-            var errorid = "error"+i;
-            var middlepart = '<div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" href="#'+errorid+'"> '+msg+'</a></h4></div>';
-            var listpart = '<ul class="list-group">';
-
-            for (var j = 0; j < vals.length; j++) {
-                listpart += '<li class="list-group-item">'+vals[j]+'</li>'
+            if (fb['error'] == true) {
+                numberOfErrors++;
             }
-            listpart += '</ul>';
-            middlepart += '<div id="'+errorid+'" class="panel-collapse collapse in">'+listpart+'</div>';
-            var html = '<div class="panel-group"><div class="panel panel-default">'+middlepart+'</div></div>'
-            $('#feedback-tables').append(html);
-        }//for
+        }
+        var numberOfWarnings = data.length - numberOfErrors;
+        if (numberOfErrors != 0) {
 
+
+            var error_word = 'errors';
+            if (data.length == 1) error_word = 'error';
+            var html_title = '<h3><span>' + numberOfErrors + ' ' + error_word + ' found... </span></h3>';
+            $('#feedback-errors').append(html_title);
+
+            for (var i = 0; i < data.length; i++) {
+                var fb = data[i];
+                if (fb['error'] == true) {
+                    var msg = fb['message'];
+                    var vals = fb['offendingValues'];
+                    var errorid = "error" + i;
+                    var middlepart = '<div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" href="#' + errorid + '"> ' + msg + '</a></h4></div>';
+                    var listpart = '<ul class="list-group">';
+
+                    for (var j = 0; j < vals.length; j++) {
+                        listpart += '<li class="list-group-item">' + vals[j] + '</li>'
+                    }
+                    listpart += '</ul>';
+                    middlepart += '<div id="' + errorid + '" class="panel-collapse collapse in">' + listpart + '</div>';
+                    var html = '<div class="panel-group"><div class="panel panel-default">' + middlepart + '</div></div>'
+                    $('#feedback-errors').append(html);
+                }
+            }//for
+        }
+        if (numberOfWarnings != 0) {
+            var error_word = numberOfWarnings + '';
+            if (numberOfWarnings == 1) {
+                error_word += ' warning found';
+            } else {
+                error_word +=' warnings found';
+            }
+            var html_title = '<h3><span>' + error_word + '</span></h3>';
+            $('#feedback-warnings').append(html_title);
+            for (var i = 0; i < data.length; i++) {
+                var fb = data[i];
+                if (fb['error'] == false) {
+                    var msg = fb['message'];
+                    var vals = fb['offendingValues'];
+                    var successId = "success" + i;
+                    var middlepart = '<div class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" href="#' + successId + '"> ' + msg + '</a></h4></div>';
+                    var listpart = '<ul class="list-group">';
+
+                    for (var j = 0; j < vals.length; j++) {
+                        listpart += '<li class="list-group-item">' + vals[j] + '</li>'
+                    }
+                    listpart += '</ul>';
+                    middlepart += '<div id="' + successId + '" class="panel-collapse collapse in">' + listpart + '</div>';
+                    var html = '<div class="panel-group"><div class="panel panel-default">' + middlepart + '</div></div>'
+                    $('#feedback-warnings').append(html);
+                }
+            }//for
+        }
     }//else
 };
 
